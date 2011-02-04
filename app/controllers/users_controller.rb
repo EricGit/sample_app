4cf11a4a85ab6b5a79f95cc2cc2de73b624bd468
Eric Git <eric.martin@sage.com>
2011-02-03 17:11:25 -0800

    Finished first cut of the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  def new
-    def new
-      @title = "Sign up"
-    end
+  def show
+    @user = User.find(params[:id])
   end
 
+  def new
+    @title = "Sign up"
+  end
 end
