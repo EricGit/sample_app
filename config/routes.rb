c14b051c930099a982931248c85b04c8fa9688aa
Eric Git <eric.martin@sage.com>
2011-02-10 16:24:13 -0800

    Finish signin/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
-  resources :users
 
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  
   #get "pages/home"
   #get "pages/contact" 
   #get "pages/about" 
@@ -9,7 +12,12 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
+
   root :to => 'pages#home'  
   
   # The priority is based upon order of creation:
